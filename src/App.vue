15ccb561402c7f3791561795fd38ab8075122c70
martin kiragu <martin@gratitudeindia.com>
2021-09-16 08:57:17 +0300

    dissable mobile support

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <div class="app flex">
+    <div v-if="!mobile" class="app flex">
       <Navigation></Navigation>
       <div class="app-content flex flex-column">
         <router-view/>
       </div>
+    </div>
+    <div v-else class="mobile-message flex flex-column">
+      <h2>Sorry, this app is not supported on Mobile Devices</h2>
+      <p>To use this app, please use a computer or a tablet</p>
     </div>
   </div>
 
@@ -14,8 +18,31 @@
 import Navigation from "./components/Navigation";
 
 export default {
+  data() {
+    return {
+      mobile: null
+    }
+
+  },
   components: {
     Navigation
+  },
+  created() {
+    /*check screen automatically hook*/
+    this.checkScreen();
+    /*list on the change screen size*/
+    window.addEventListener('resize', this.checkScreen)
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 750) {
+        this.mobile = true;
+        return
+
+      }
+      this.mobile = false;
+    }
   }
 }
 </script>
@@ -38,8 +65,21 @@ export default {
     flex-direction: row !important;
   }
 }
-.app-content
-{
+
+.mobile-message{
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #141625;
+  color: #ffffff;
+
+  p{
+    margin-top: 16px;
+  }
+}
+
+.app-content {
   padding: 0 20px;
   flex: 1;
   position: relative;
