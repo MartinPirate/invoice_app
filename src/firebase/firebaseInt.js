e18ac4f8789586eaa9c176977d3391042e0f3ee1
martin kiragu <martin@gratitudeindia.com>
2021-09-23 05:54:42 +0300

    firebase and firestore configurations

---
+++ src/firebase/firebaseInt.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+import "firebase/firestore"
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCFkFbe-xX3BdDWy9cSXos66WdVd31tzAI",
+    authDomain: "invoiceapp-4153c.firebaseapp.com",
+    projectId: "invoiceapp-4153c",
+    storageBucket: "invoiceapp-4153c.appspot.com",
+    messagingSenderId: "1023017182025",
+    appId: "1:1023017182025:web:790f1c758ad92e48af7098"
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
